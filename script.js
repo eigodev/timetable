d876eb10b94e2be81d137ee06e0d441371c66631
eigodev <eigo_developer@outlook.com>
2026-05-30 10:03:13 -0300

    Update ADMIN

--- script.js
+++ script.js
@@ -12353,10 +12353,11 @@ function buildStudentWhatsappMessage(studentName) {
     ];
     const classLinkLine = classLinkLineOptions[Math.floor(Math.random() * classLinkLineOptions.length)] || "Here's your class link.";
     return `${dateLine}
-${greeting}, ${firstName}. ${followUp}
+    ${greeting}, '@'. ${followUp}
 
-${meetLine}
-${classLinkLine}`;
+    ${meetLine}
+    ${classLinkLine}
+    `;
 }
 
 function refreshEditStudentSchoolSelect(selectedSchool = '') {
