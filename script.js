f2caa1155aabb0c9f79c34d7c36e9422b5522cdd
eigodev <eigo_developer@outlook.com>
2026-06-01 08:07:30 -0300

    WhatsApp Message

--- script.js
+++ script.js
@@ -12441,7 +12441,7 @@ function buildStudentWhatsappMessage(studentName) {
     ];
     const classLinkLine = classLinkLineOptions[Math.floor(Math.random() * classLinkLineOptions.length)] || "Here's your class link.";
     return `${dateLine}
-    ${greeting}, '@'. ${followUp}
+    ${greeting}, @. ${followUp}
 
     ${meetLine}
     ${classLinkLine}
